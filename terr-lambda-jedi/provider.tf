bfc722f81327f6ec30ddf7b9375315f0e32191ac
Fariba <113408923+frbmohammaditabar@users.noreply.github.com>
2023-12-18 14:15:53 +0330

    Update provider.tf

--- terr-lambda-jedi/provider.tf
+++ terr-lambda-jedi/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAUSBC7FPAMZARS5AT"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
